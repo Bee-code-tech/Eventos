7ccc0333a46b8cc221cc2a68026290c70013e8cc
Bee-code-tech <babawaleolatunji64@gmail.com>
2024-09-30 02:39:34 +0100

    okay

--- src/pages/Events/Events.tsx
+++ src/pages/Events/Events.tsx
@@ -4,7 +4,7 @@ import EventList from "@/components/EventList/EventCard";
 const Events = () => {
   return (
     <div className="container flex flex-col items-center min-h-screen px-4 mx-auto my-24 mb-24">
-      <h1 className="mb-4 text-2xl font-bold">All Events</h1>
+      <h1 className="mb-8 text-2xl font-bold ">All Events</h1>
       <EventList /> {/* Display all events */}
     </div>
   );
